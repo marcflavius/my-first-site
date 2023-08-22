8637a3f9c277b7b3b068cdad9a580bc9b83f122f
Marc FLAVIUS <marc_flavius@yahoo.fr>
2023-08-22 23:28:27 +0200

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `My First site `,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
